363fc5ce8445f56b802dd145b06d27184261c8fc
chathuradevinda <avilon.devinda@gmail.com>
2020-05-29 10:04:35 +0530

    simplified code

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotificationsComponent } from './notifications/notifications.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { TimelineComponent } from './timeline/timeline.component';
 
 @NgModule({
-  declarations: [NotificationsComponent],
+  declarations: [NotificationsComponent, StatisticsComponent, TimelineComponent],
   imports: [
     CommonModule
   ]
--- src/app/modules/participants/participants.module.ts
+++ src/app/modules/participants/participants.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddParticipantsModule } from './add-participants/add-participants.module';
-import { EditParticipantsComponent } from './edit-participants/edit-participants.component';
+import { EditParticipantsComponent } from './list-participants/edit-participants.component';
 
 @NgModule({
   declarations: [
