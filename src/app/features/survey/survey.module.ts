20851fa06c9b7a9a0c8f8375b063a4db4efbdeaf
chathuradevinda <avilon.devinda@gmail.com>
2020-06-01 19:07:44 +0530

    added prime ng

---
+++ src/app/features/survey/survey.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {CardModule} from 'primeng/card';
+
+@NgModule({
+  declarations: [
+
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SurveyModule { }
